229b7779bb6d0ebb2cb121dd1701b5abb68c84cb
imilienko <imilienko@intech-global.com>
2022-05-20 22:59:52 +0500

    improving tests; try to find matches

--- match-three/src/main/model.js
+++ match-three/src/main/model.js
@@ -1,3 +1,5 @@
+import {re} from "@babel/core/lib/vendor/import-meta-resolve";
+
 export class MatchThreeModel {
     constructor(size) {
         this._gameField = [];
@@ -13,32 +15,36 @@ export class MatchThreeModel {
         }
     }
 
+    _findMatchedIndices(row) {
+        let base = row[0];
+        let indices = [0];
+        console.log("Base is ", base,"\nIndices:",indices,"\n=======");
+        row.slice(1).forEach((el, j) => {
+            console.log("Base is ", base,"\nIndices:",indices,"\n=======");
+            if (el !== base) {
+                if (indices.length < 3) indices.splice(0);
+                base = el;
+            }
+            indices.push[j + 1];
+        });
+        return indices;
+    }
+
     deleteMatched() {
-        let hasMatches = false;;
+        let hasMatches = false;
+        ;
         //check rows
-        this._gameField.forEach(row => {
-            let base = row[0];
-            let indices = [0];
-
-            row.slice(1).forEach((el, j) => {
-                if (el === base) {
-                    indices.push(j + 1)
-                } else {
-                    if (indices.length < 3) indices = [];
-                    base = el;
-                }
-            })
-
+        this._gameField.forEach((function (row) {
+            const indices = this._findMatchedIndices(row)
             if (indices.length > 0) {
                 indices.forEach(idx => {
                     row[idx] = 0;
                 });
                 hasMatches = true;
             }
-        })
+        }).bind(this))
         return hasMatches;
     }
-
 
 
 }
--- match-three/src/test/model.test.js
+++ match-three/src/test/model.test.js
@@ -1,4 +1,15 @@
-import { MatchThreeModel } from "../main/model";
+import {MatchThreeModel} from "../main/model";
+
+test("Find matches un row", () => {
+    const model = new MatchThreeModel({width: 10, height: 10});
+    //           0  1  2  3  4  5  6 [7  8  9]10[11 12 13 14]15 16 17
+    const row = [1, 1, 2, 2, 3, 4, 5, 2, 2, 2, 3, 1, 1, 1, 1, 3, 3, 2];
+    const indices = model._findMatchedIndices(row);
+    console.log(indices);
+    expect(indices).toEqual([7,8,9,11,12,13,14])
+
+});
+
 // todo написать тест и реализацию рекурсивного обхода по клику
 test("Create 5 to 5 matrix filled with digits 1 to 3", () => {
     const model = new MatchThreeModel({
@@ -13,25 +24,45 @@ test("Create 5 to 5 matrix filled with digits 1 to 3", () => {
     })
 });
 
-test("Test delete matched",
-    () => {
-        const input = [
-            [1,2,3,3,2],
-            [3,2,1,1,2],
-            [3,3,3,1,3],
-            [1,2,3,3,2],
-            [3,2,1,3,2],
-        ];
-        const model = new MatchThreeModel({width:5, height:5})
-        model._gameField = input;
-        expect(model.deleteMatched()).toBe(true);
-        expect(model._gameField).toEqual(
-            [
-                [1,2,3,3,2],
-                [3,2,1,1,2],
-                [0,0,0,1,3],
-                [1,2,3,3,2],
-                [3,2,1,3,2],
-            ]
-        )
-    })
+// test("Test delete matched",
+//     () => {
+//         const input = [
+//             [1, 2, 3, 3, 2],
+//             [3, 2, 1, 1, 2],
+//             [3, 3, 3, 1, 3],
+//             [1, 2, 3, 3, 2],
+//             [3, 2, 1, 3, 2],
+//         ];
+//         let model = new MatchThreeModel({width: 5, height: 5})
+//         model._gameField = input;
+//         expect(model.deleteMatched()).toBe(true);
+//         expect(model._gameField).toEqual(
+//             [
+//                 [1, 2, 3, 3, 2],
+//                 [3, 2, 1, 1, 2],
+//                 [0, 0, 0, 1, 3],
+//                 [1, 2, 3, 3, 2],
+//                 [3, 2, 1, 3, 2],
+//             ]
+//         )
+//
+//         const input2 = [
+//             [1, 2, 3, 3, 2, 3, 1, 2, 1, 3, 2, 3, 3],
+//             [3, 2, 1, 1, 2, 2, 2, 1, 2, 1, 1, 1, 1],
+//             [3, 3, 3, 1, 3, 3, 3, 1, 2, 3, 1, 2, 3],
+//             [1, 2, 3, 3, 2, 3, 3, 1, 2, 3, 1, 2, 3],
+//             [3, 2, 1, 3, 2, 3, 3, 1, 2, 3, 1, 2, 3],
+//         ];
+//         model = new MatchThreeModel({width: 5, height: 5})
+//         model._gameField = input2;
+//         expect(model.deleteMatched()).toBe(true);
+//         expect(model._gameField).toEqual(
+//             [
+//                 [1, 2, 3, 3, 2, 3, 1, 2, 1, 3, 2, 3, 3],
+//                 [3, 2, 1, 1, 0, 0, 0, 1, 2, 0, 0, 0, 0],
+//                 [0, 0, 0, 1, 0, 0, 0, 1, 2, 3, 1, 2, 3],
+//                 [1, 2, 3, 3, 2, 3, 3, 1, 2, 3, 1, 2, 3],
+//                 [3, 2, 1, 3, 2, 3, 3, 1, 2, 3, 1, 2, 3],
+//             ]
+//         )
+//     })
